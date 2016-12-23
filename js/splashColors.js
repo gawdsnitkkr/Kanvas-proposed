26a285c2597bc134b6a58cd2a729846f0eb0deb6
abhishekg785 <abhishekg785@gmail.com>
2016-12-23 22:51:34 +0530

    animation time set

--- js/splashColors.js
+++ js/splashColors.js
@@ -9,23 +9,41 @@
     var canvas,
         ctx,
         blend = 100,
+        time = 0,
+        animationTime, // time for which the aimation of splash color will run
+        doAnimation = true,
         _Functions = {
 
             /*
              generates random hex color values each second
              */
             CreateAnimation : function() {
-                w.requestAnimationFrame(_Functions.CreateAnimation);
-                var px = Math.floor(Math.random() * canvas.width);
-                var py = Math.floor(Math.random() * canvas.height);
-                var pr = Math.floor(Math.random() * 50);
-                var pCol = {
-                    'r' : Math.floor(Math.random() * 255),
-                    'g' : Math.floor(Math.random() * 255),
-                    'b' : Math.floor(Math.random() * 255),
-                    'a' : 1 / blend
-                };
-                _Functions.SpreadColors(px, py, pr, pCol);
+                function hailAnimate() {
+                    console.log('running animation');
+                    console.log(time);
+                    if(doAnimation) {
+                        w.requestAnimationFrame(hailAnimate);
+                        var px = Math.floor(Math.random() * canvas.width);
+                        var py = Math.floor(Math.random() * canvas.height);
+                        var pr = Math.floor(Math.random() * 50);
+                        var pCol = {
+                            'r' : Math.floor(Math.random() * 255),
+                            'g' : Math.floor(Math.random() * 255),
+                            'b' : Math.floor(Math.random() * 255),
+                            'a' : 1 / blend
+                        };
+                        _Functions.SpreadColors(px, py, pr, pCol);
+                        if(time == animationTime) {
+                            w.cancelAnimationFrame(hailAnimate);
+                            doAnimation = false;
+                            return;
+                        }
+                        time++;
+                    }
+                }
+                if(doAnimation == true) {
+                    hailAnimate();
+                }
             },
 
             SpreadColors : function(px, py, pr, pCol) {
@@ -36,12 +54,14 @@
                     col = pCol;
 
                 function ink() {
-                    w.requestAnimationFrame(ink);
-                    _Functions.CreateSpot(x, y, r * (i / blend), col);
-                    if(i == blend) {
-                        w.cancelAnimationFrame(ink);
+                    if(doAnimation) {
+                        w.requestAnimationFrame(ink);
+                        _Functions.CreateSpot(x, y, r * (i / blend), col);
+                        if(i == blend) {
+                            w.cancelAnimationFrame(ink);
+                        }
+                        i++;
                     }
-                    i++;
                 }
                 ink();
 
@@ -53,6 +73,24 @@
                 ctx.beginPath();
                 ctx.arc(x, y, r, 0, Math.PI * 2, false);
                 ctx.fill();
+            },
+
+            /*
+            *   the request animation frame for canvas created shit bro :P
+            *   so have to put some limit on splashing the colors on the screen
+            *   after all it is running on a browser :P
+            */
+            SetTimeForAnimation : function() {
+                if(w.innerWidth <= 500) {
+                    animationTime = 500;
+                }
+                else if(w.innerWidth > 500 && w.innerWidth < 1000) {
+                    animationTime = 800;
+                }
+                else {
+                    animationTime = 1000;
+                }
+                console.log(animationTime);
             }
         }
 
@@ -63,6 +101,7 @@
      *   create circles for the colors on the canvas
      */
     $(w).on('load', function() {
+        _Functions.SetTimeForAnimation();
         canvas = document.getElementById('canv');
         ctx = canvas.getContext('2d');
         canvas.height = w.innerHeight;
@@ -73,6 +112,8 @@
     $(w).on('resize', function() {
         canvas.width = w.innerWidth;
         canvas.height = w.innerHeight;
+        _Functions.SetTimeForAnimation();
     });
+
 
 })(jQuery, window, document);
