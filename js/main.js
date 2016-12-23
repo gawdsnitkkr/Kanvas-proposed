2dde3d0be38b26b91d5e0f2f3b9b21f895406cb0
abhishekg785 <abhishekg785@gmail.com>
2016-12-24 04:52:41 +0530

    content added

--- js/main.js
+++ js/main.js
@@ -3,18 +3,18 @@
  *
  *  main.js
  */
-// (function($, w, d) {
-//
-//     /*
-//     *  if clicked on the link in the nav bar simply scroll
-//     *  to the specified div having delay of 1s
-//     */
-//
-//     $('.nav-bar a').on('click', function(e) {
-//         e.preventDefault();
-//         $('html, body').animate({
-//             scrollTop : $($(this).attr('href')).offset().top
-//         },500);
-//     });
-//
-// })(jQuery, window, document);
+(function($, w, d) {
+
+    /*
+    *  if clicked on the link in the nav bar simply scroll
+    *  to the specified div having delay of 1s
+    */
+
+    $('.nav-bar a').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop : $($(this).attr('href')).offset().top
+        },500);
+    });
+
+})(jQuery, window, document);
